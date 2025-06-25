156e37ce0580fde33c9ab12c99f79811838679a3
dongha <donghak937@gmail.com>
2025-06-25 23:41:20 +0900

    finish A001

--- A001/A001.cpp
+++ A001/A001.cpp
@@ -7,22 +7,18 @@ using namespace std;
 int main(){
     vector<int> g = {1,2,3};
     vector<int> s = {3};
-    sort(g.begin(), g.end());
     int i = 0, j = 0, count = 0;
     if (g.size () == 0 || s.size() == 0) return 0;
-    
-    while (g[g.size() - 1] != 0 && s[s.size() - 1] != 0){
-        while (g[i] != 0 && j < s.size() - 1){
-            if (s[j] >= g[i]){
-                g[i] = 0;
-                s[j] = 0;
-                j = 0;
-                count++;
-                break;
-            }
-            j++;
+    sort(g.begin(), g.end());
+    sort(s.begin(), s.end());
+
+    while (i < g.size() && j < s.size()){
+        if (g[i] <= s[j]){
+            i++;
+            count++;
         }
-        i++;
+        j++;
     }
+    return count;
     cout << count;
 }
