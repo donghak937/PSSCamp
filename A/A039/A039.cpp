b2dad70a30c127aaada581caa6db30f1aa8f224b
dongha <donghak937@gmail.com>
2025-07-07 23:05:41 +0900

    039, 038

--- A/A039/A039.cpp
+++ A/A039/A039.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main(){
-    int x = 2147395599;
+    int x = 14;
     double answer = x;
 
     if (x == 1 || x == 0) return x;
 
     for(int i = 0; i < 100; i++){
         answer = (answer + x / answer) / 2;
-        cout << (int)answer << endl;
     }
-    
+    if ((int)answer == answer) cout << true;
+    else cout << false;
 } 
