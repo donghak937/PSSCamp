0e4d199f5df527718203d89a8af9ec25b095c0d6
dongha <donghak937@gmail.com>
2025-07-07 22:35:51 +0900

    add 038

---
+++ A/A038/A038.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x = 2147395599;
+    double answer = x;
+
+    if (x == 1 || x == 0) return x;
+
+    for(int i = 0; i < 100; i++){
+        answer = (answer + x / answer) / 2;
+        cout << (int)answer << endl;
+    }
+    
+} 
