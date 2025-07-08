8689986a704b9db4442a2d85530b42c9759a9494
dongha <donghak937@gmail.com>
2025-07-09 00:10:46 +0900

    add

--- A/A032/A032.cpp
+++ A/A032/A032.cpp
@@ -13,23 +13,15 @@ int main(){
         a[0][i] = i;
         a[i][0] = 0;
     }
-    
-
     for(int i = 1; i < 15; i++){
         for(int j = 1; j < 15; j++){
             a[i][j] = a[i][j - 1] + a[i - 1][j];
         }
     }
-
-
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> floor >> room;
 
         cout << a[floor][room] << endl;
     }
-
-    
-
-
 } 
