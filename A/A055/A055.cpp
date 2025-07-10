3ad63daa0c800b0e7f66351ac8bf005cd610c886
dongha <donghak937@gmail.com>
2025-07-10 12:51:27 +0900

    add 55 but need to fix

---
+++ A/A055/A055.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    stack<char> a;
+    string s = "(()())(())(()(()))";
+    for(int i = 0; i < s.size(); i++){
+        a.push(s[i]);
+    }
+
+    
+
+
+
+}
