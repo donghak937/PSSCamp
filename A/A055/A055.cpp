d2d3e3e17b51be9d2269d5d0f313815ae7544448
dongha <donghak937@gmail.com>
2025-07-10 14:08:08 +0900

    55 finish

--- A/A055/A055.cpp
+++ A/A055/A055.cpp
@@ -1,17 +1,49 @@
 #include <iostream>
-#include <stack>
-
 using namespace std;
 
 int main() {
-    stack<char> a;
-    string s = "(()())(())(()(()))";
-    for(int i = 0; i < s.size(); i++){
-        a.push(s[i]);
+    string answer = "";
+    int i = 0;
+    int left = 0;
+    int right = 0;
+    int leftPos = 0;
+    int rightPos = 0;
+    string s = "(()())(())";
+    while(i < s.size()){
+        if(s[i] == '('){
+            left = 0;
+            leftPos = i;
+
+            do {
+                if (s[i] == '(') left++;
+
+                else right++; 
+
+                #ifdef Debug
+                cout << "left : " << left << " right : " << right << endl;
+                #endif
+                
+                i++;
+                
+            } while (left != right);
+
+            right = 0;
+            left = 0;
+            rightPos = i - 1;
+        }
+
+        #ifdef Debug
+        cout << "leftPos : " << s[leftPos]  << " " << leftPos << " rightPos : " << s[rightPos] << " " << rightPos << endl;
+        #endif
+
+        s.erase(rightPos, 1);
+        s.erase(leftPos, 1); 
+
+        #ifdef Debug
+        cout << s << endl;
+        #endif
+
+        i -= 2;
     }
-
-    
-
-
-
+    cout << "answer : " << s;
 }
