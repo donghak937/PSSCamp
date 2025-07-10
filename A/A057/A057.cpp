102770c1eed101c70eb4301351c436de4fe35c93
dongha <donghak937@gmail.com>
2025-07-10 21:54:37 +0900

    add57

---
+++ A/A057/A057.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> cookie = {1,2,1,2};
+
+    if (cookie.size() == 1) return 0; 
+    if (cookie.size() == 2) { 
+        if (cookie[0] == cookie[1]) return cookie[0];
+        return 0; 
+    }
+
+    int bro = 0;
+    int lilbro = 0;
+    
+    int M;
+    int C;
+
+    int max = 0;
+
+    for(int i = 0; i < cookie.size() - 1; i++){
+        M = i;
+        C = M + 1;
+        bro = cookie[M];
+        lilbro = cookie[C];
+        while (true){
+            if ((lilbro == bro) && max <= bro){
+                max = bro;
+            }
+            else if ((bro <= lilbro) && M != 0){
+                M--;
+                bro += cookie[M];
+            }
+            else if ((bro >= lilbro) && C != cookie.size() -1 ){
+                C++;
+                lilbro += cookie[C];
+            }
+            else{
+                break;
+            }
+        }
+    }
+    cout << max;
+
+}
