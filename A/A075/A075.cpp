c5797236b6d926903eff23a4047c7b5dfecc79b0
dongha <donghak937@gmail.com>
2025-07-14 01:30:21 +0900

    75

---
+++ A/A075/A075.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+bool comps(int a, int b){
+    string a2 = to_string(a);
+    string b2 = to_string(b);
+
+    for(int i = 0; i < 3; i++){
+        a2 += a2;
+        b2 += b2;
+    }
+    return a2 + b2 > b2 + a2;
+}
+
+int main (){
+    vector<int> numbers = {0,0,0,0};
+    string answer = "";
+
+    sort(numbers.begin(), numbers.end(), comps);
+
+    for (int num : numbers) {
+        
+        answer += to_string(num);
+    }
+    if (answer[0] == '0') answer = "0";
+    cout << answer;
+}
