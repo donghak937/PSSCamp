45bedca57e25e62a7ae711fc681f9d0a4d0b3d9c
dongha <donghak937@gmail.com>
2025-07-14 01:32:16 +0900

    75

--- A/A075/A075.cpp
+++ A/A075/A075.cpp
@@ -8,11 +8,6 @@ using namespace std;
 bool comps(int a, int b){
     string a2 = to_string(a);
     string b2 = to_string(b);
-
-    for(int i = 0; i < 3; i++){
-        a2 += a2;
-        b2 += b2;
-    }
     return a2 + b2 > b2 + a2;
 }
 
