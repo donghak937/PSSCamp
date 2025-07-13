23c20a1c26224ac6c230dcc069e0fce3e1a52c27
dongha <donghak937@gmail.com>
2025-07-14 03:32:50 +0900

    80

---
+++ A/A080/A080.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+  
+int main() {
+    vector<int> nums = {2,2,1,1,1,2,2};
+    int max = *max_element(nums.begin(), nums.end());
+    int N = nums.size() / 2;
+    int answer = 0;
+    for(int a : nums){
+        int counts = count(nums.begin(), nums.end(), a);
+        if (counts > N) cout << a; 
+    }
+}
