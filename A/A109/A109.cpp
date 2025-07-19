66355a82e91a61a86a5591c8dec7b551f7bc698c
dongha <donghak937@gmail.com>
2025-07-19 15:23:08 +0900

    109

--- A/A109/A109.cpp
+++ A/A109/A109.cpp
@@ -1,19 +1,19 @@
-    #include <iostream>
-    #include <algorithm>
-    #include <string>
+#include <iostream>
+#include <algorithm>
+#include <string>
 
-    using namespace std;
+using namespace std;
 
-    int main() {
-        int N;
-        cin >> N;
-        int tmp1, tmp2, tmp3;
-        int rot = 0;
-        int ans = 1;
-        for(int i = 0; i < N; i++){
-            cin >> tmp1 >> tmp2 >> tmp3;
-            ans = (ans * tmp1) / tmp2;
-            rot ^= tmp3;
-        }
-        cout << rot << " " << ans;
+int main() {
+    int N;
+    cin >> N;
+    int tmp1, tmp2, tmp3;
+    int rot = 0;
+    int ans = 1;
+    for(int i = 0; i < N; i++){
+        cin >> tmp1 >> tmp2 >> tmp3;
+        ans = (ans / tmp1) * tmp2;
+        rot ^= tmp3;
     }
+    cout << rot << " " << ans;
+}
