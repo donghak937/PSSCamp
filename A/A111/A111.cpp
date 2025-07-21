423369de69d2de16d8351e302bb95f65d70ccc87
dongha <donghak937@gmail.com>
2025-07-21 23:53:34 +0900

    111

--- A/A111/A111.cpp
+++ A/A111/A111.cpp
@@ -7,33 +7,21 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    int mon = 1000 - N;
-    int count = 0;
-    while(true){
-        if (mon / 500 != 0){
-            mon -= 500;
+    for(int i = 0; i < N; i++){
+        int how;
+        cin >> how;
+        int tmp = 1;
+        int count = 0;
+        int half = 0;
+        while (count != how) {
+            tmp *= 2;
+            half++;
+            if (half == 2){
+                tmp += 1;
+                half = 0;
+            }
             count++;
         }
-        else if (mon / 100 != 0){
-            mon -= 100;
-            count++;
-        }
-        else if (mon / 50 != 0){
-            mon -= 50;
-            count++;
-        }
-        else if (mon / 10 != 0){
-            mon -= 10;
-            count++;
-        }
-        else if (mon / 5 != 0){
-            mon -= 5;
-            count++;
-        }
-        else {
-            count += mon;
-            break;
-        }
+        cout << count << endl; 
     }
-    cout << count;
 }
