2a954e423b55d31aa73daed2594b37426929c62c
dongha <donghak937@gmail.com>
2025-07-22 18:04:23 +0900

    115

--- A/A115/A115.cpp
+++ A/A115/A115.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    int total = 0;
-    int num;
-    for(int i = 0; i < 5; i ++){
-        cin >> num;
-        total += num * num;
+    string text = "nlaebolko";
+    int a = 0;
+    int b = 0;
+    int l = 0;
+    int n = 0;
+    int o = 0;
+    int answer = 0;
+    for(char ch : text){
+        if (ch == 'b') b++;
+        else if (ch == 'a') a++;
+        else if (ch == 'l') l++;
+        else if (ch == 'o') o++;
+        else if (ch == 'n') n++;
     }
-    cout << total % 10;
+    while (true) {
+        b--;
+        if (b < 0) break;
+        a--; 
+        if (a < 0) break;
+        l -= 2; 
+        if (l < 0) break;
+        o -= 2; 
+        if (o < 0) break;
+        n--; 
+        if (n < 0) break;
+        answer++;
+    }
+    cout << answer;
+
 }
