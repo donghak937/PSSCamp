67aacd1df4cbd8dc041c6bb90bd05fef46723a15
dongha <donghak937@gmail.com>
2025-08-13 22:52:35 +0900

    add

--- baekjoon/1978.cpp
+++ baekjoon/1978.cpp
@@ -11,7 +11,7 @@ int main() {
         int n;
         cin >> n;
         if (n == 0 || n == 1 || n == 2) continue;
-        else{
+        else{ 
             for(int i = 1; i < n / 2; i++){
                 if (n % i == 0){
                     isOk = false;
