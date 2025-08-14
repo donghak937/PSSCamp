d91376a2f90628c2f262dba7a55058d7ef423873
dongha <donghak937@gmail.com>
2025-08-14 22:52:42 +0900

    add

--- baekjoon/1978.cpp
+++ baekjoon/1978.cpp
@@ -14,7 +14,7 @@ int main() {
         else{ 
             for(int i = 1; i < n / 2; i++){
                 if (n % i == 0){
-                    isOk = false;
+                    isOk = false; 
                     cout << i;
                     break;
                 }   
