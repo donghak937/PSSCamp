944104981b7bd842e5486448bc1998ebca2a3cd5
dongha <donghak937@gmail.com>
2025-08-31 10:02:01 +0900

    add

--- baekjoon/1978.cpp
+++ baekjoon/1978.cpp
@@ -18,7 +18,7 @@ int main() {
                     cout << i;
                     break;  
                 }   
-            }
+            } 
             if (isOk) count++; 
         }   
     }  
