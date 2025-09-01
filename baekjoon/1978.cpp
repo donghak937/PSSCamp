bea7712c9af05de8019cd2b04a00a90df897152d
dongha <donghak937@gmail.com>
2025-09-01 10:20:18 +0900

    add

--- baekjoon/1978.cpp
+++ baekjoon/1978.cpp
@@ -3,23 +3,23 @@
 using namespace std;
 
 int main() {
-    int N;  
+    int N;
     int count = 0;
     cin >> N;
     for(int i = 0; i < N; i++){
         bool isOk = true;
         int n;
         cin >> n;
-        if (n == 0 || n == 1 || n == 2) continue;
-        else{   
-            for(int i = 1; i < n / 2; i++){
+        if (n == 0 || n == 1) continue;
+        else{
+            for(int i = 2; i < n; i++){
                 if (n % i == 0){
-                    isOk = false; 
-                    cout << i;
-                    break;  
-                }   
-            } 
-            if (isOk) count++; 
-        }   
-    }  
-} 
+                    isOk = false;
+                    break;
+                }
+            }
+            if (isOk) count++;
+        }
+    }
+    cout << count;
+}
