de9b9b7a1ffffc3a6ebf49f32283f0873cea1b4c
dongha <donghak937@gmail.com>
2025-09-01 22:25:58 +0900

    add

--- baekjoon/9996.cpp
+++ baekjoon/9996.cpp
@@ -9,19 +9,19 @@ int main() {
     cin >> N;
     cin >> str;
     bool isOk = true;
-    string pre;
-    string fore;
+    string pre ="";
+    string fore="";
 
     for(int i = 0; i < str.size(); i++){
-        if (str[i] == '*')isOk = false;
+        if (str[i] == '*') isOk = false;
         else if (isOk) pre += str[i];
-        else fore += str[i];
+        else fore += str[i];    
     }
-
+ 
     for(int i = 0; i < N; i++){
         string a;
         cin >> a;
-        if (a.size() < pre.size() || a.size() < fore.size()) cout << "NE" << endl;
+        if (a.size() < pre.size() + fore.size()) cout << "NE" << endl;
         else if (a.substr(0, pre.size()) == pre && a.substr(a.size() - fore.size(), fore.size()) == fore) cout << "DA" << endl;
         else cout << "NE" << endl;
     }
