cebddc519fd35368b8c5fed5204151358e7d7568
dongha <donghak937@gmail.com>
2025-09-01 13:18:57 +0900

    add

--- baekjoon/3009.cpp
+++ baekjoon/3009.cpp
@@ -13,7 +13,7 @@ int main() {
 
     if (x1 == x2) cout << x3 << " ";
     else if (x1 == x3) cout << x2 << " ";
-    else cout << x3 << " ";
+    else cout << x1 << " ";
 
     if (y1 == y2) cout << y3;
     else if (y1 == y3) cout << y2;
