884cb85b7b166f08417906024c65897d6ea96c0d
dongha <donghak937@gmail.com>
2025-09-03 20:50:07 +0900

    add

---
+++ baekjoon/14645.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            int a;
+            cin >> a;
+        }
+    }
+
+    cout << "비와이";
+
+    //그냥 비와이 해도 맞음 ㅋㅋ
+
+}
