3ae2ede29275f9405914ddcf16c60078552079b0
dongha <donghak937@gmail.com>
2025-09-05 10:33:05 +0900

    add

--- baekjoon/14626.cpp
+++ baekjoon/14626.cpp
@@ -14,13 +14,28 @@ int main() {
         if (a[i] == '*'){
             if (i % 2 != 0) isOk = false;
         }
-        else if (i % 2 == 0 || i == 0) total += a[i] - '0';
-        else total += (a[i] - '0') * 3;
+        else if (i % 2 == 0 || i == 0) {
+            total += a[i] - '0';
+        }
+        else {
+            total += (a[i] - '0') * 3;
+        }
     }
+    total += a[12] - '0';
 
-    total %= 10;
-    m -= total;
-
-    if(isOk) cout << m;
-    else cout << m/3;
+    for(int i = 1; i < 10; i++){
+        if (!isOk){
+            if ((total + i * 3) % 10 == 0){
+                cout << i;
+                return 0;
+            }
+        }
+        else{
+            if((total + i) % 10 == 0){
+                cout << i;
+                return 0;
+            }
+        }
+    }
+    cout << "0";
 }
