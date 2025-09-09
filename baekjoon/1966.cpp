e1ea5997238faa3c29cc197ddfc2c07a3dc93d4e
dongha <donghak937@gmail.com>
2025-09-09 14:35:32 +0900

    add

---
+++ baekjoon/1966.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <  >
+
+using namespace std;
+
+int main() {
+    int size;
+    cin >> size;
+    for(int i = 0; i < size; i ++){
+        int n, m;
+        cin >> n >> m;
+        queue<int> q;
+        for(int j = 0; j < n; j++){
+            int a;
+            cin >> a;
+            q.push(a);
+        }
+    }
+}
