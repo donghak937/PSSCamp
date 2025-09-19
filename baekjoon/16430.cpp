2d8a499824496933277d17e72b7e536f95e6a809
dongha <donghak937@gmail.com>
2025-09-19 23:39:20 +0900

    add

--- baekjoon/16430.cpp
+++ baekjoon/16430.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+    #include <iostream>
 
-using namespace std;
+    using namespace std;
 
-int main() {
-    int a, b;
-    cin >> a >> b;
+    int main() {
+        int a, b;
+        cin >> a >> b;
 
-    cout << b - a << " " <<b;
-}
+        cout << b - a << " " <<b;
+    }
