b17dec5d23fabc801f94387d64a659b159370c25
dongha <donghak937@gmail.com>
2025-09-23 14:31:21 +0900

    예의주시

--- baekjoon/1966.cpp
+++ baekjoon/1966.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
@@ -9,11 +10,30 @@ int main() {
     for(int i = 0; i < size; i ++){
         int n, m;
         cin >> n >> m;
-        queue<int> q;
+        int saved = 0;
+        queue<pair<int, int>> q;
+        priority_queue<int> pq;
         for(int j = 0; j < n; j++){
             int a;
             cin >> a;
-            q.push(a);
+            q.push({j, a});
+            pq.push(a);
         }
+
+        while (!pq.empty()) {
+            int t = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            if (pq.top() == y){
+                saved++;
+                pq.pop();
+                if(t == m){
+                    cout << saved << '\n';
+                    break;
+                }
+            }
+            else q.push({t, y});
+        }
+        
     }
 }
