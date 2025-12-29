77a984f65330d42c09feed2f920274e319ba24b8
dongha <donghak937@gmail.com>
2025-12-29 23:16:56 +0900

    add but hard

---
+++ baekjoon/1149.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int red[2000];
+    int green[2000];
+    int blue[2000];
+
+    int dp[2000];
+
+    for(int i = 0; i < n; i++){
+        int a,b,c;
+        cin >> a >> b >> c;
+        red[i] = a;
+        green[i] = b;
+        blue[i] = c;
+    }
+
+    for(int i = 0; i < n; i++){
+        
+        dp[]
+
+    }
+        
+}
