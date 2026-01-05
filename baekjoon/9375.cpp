d88ddf1491f9babd3909d666176cc4a228721289
dongha <donghak937@gmail.com>
2026-01-05 21:20:22 +0900

    add

---
+++ baekjoon/9375.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+
+    int n, m;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> m;
+        map<string, string> cloth;
+        for(int j = 0; j < m; j++){
+            string clot, types;
+            cin >> clot >> types;
+            
+        }
+    }
+
+}
