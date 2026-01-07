f4d028581ea48466da5210f20c4fa2c0e29363bf
dongha <donghak937@gmail.com>
2026-01-08 08:14:55 +0900

    add

--- A/A135.cpp
+++ A/A135.cpp
@@ -8,14 +8,13 @@ int main() {
     int n;
     map<string, int> cnt;
     cin >> n;
-    int mx;
+    int mx = 0;
     for(int i = 0; i < n; i ++){
         string s;
-
         cin >> s;
-        mx = max(mx, cnt[s]++);
-
+        mx = max(mx, ++cnt[s]);
     }
+
     for (auto &p : cnt) {
         if (p.second == mx) {
             cout << p.first << "\n";
