bb01c8d1dee27a9cfb16ee8f5cf021481bd70f9e
dongha <donghak937@gmail.com>
2026-01-28 12:45:23 +0900

    add

---
+++ A/A195.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int number;
+    cin >> number;
+    for (int i = 0; i < number; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
