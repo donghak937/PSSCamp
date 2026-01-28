95fc4a55cbad0ef2d3b3134df5dbc3599afd8582
dongha <donghak937@gmail.com>
2026-01-28 10:36:44 +0900

    add

--- A/A157.cpp
+++ A/A157.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int N, C;
     cin >> N >> C;
---
+++ A/A158.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    vector<string> players = {"mumu", "soe", "poe", "kai", "mine"};
+    vector<string> callings = {"kai", "kai", "mine", "mine"};
+    vector<string> answer;
+
+}
+
