9a91065711b959e8896ad4c664c80e6113247221
dongha <donghak937@gmail.com>
2026-01-28 10:53:23 +0900

    add

--- A/A158.cpp
+++ A/A158.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
@@ -9,7 +11,29 @@ int main() {
 
     vector<string> players = {"mumu", "soe", "poe", "kai", "mine"};
     vector<string> callings = {"kai", "kai", "mine", "mine"};
-    vector<string> answer;
+
+    unordered_map<string, int> pos;
+
+    for (int i = 0; i < (int)players.size(); i++) {
+        pos[players[i]] = i;
+    }
+    
+    for(int i = 0; i < callings.size(); i++){
+        
+        int idx = pos[callings[i]];
+        
+        auto it  = players.begin() + idx;
+        auto it2 = it - 1;
+        
+        string frontName = *it2;
+        string curName   = *it;
+
+        iter_swap(it, it2);
+
+        pos[curName] = idx - 1;
+        pos[frontName] = idx;
+    }
+
 
 }
 
