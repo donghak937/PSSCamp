168298c5ab82f7c0a27a9848253f3cfeefd34fb6
dongha <donghak937@gmail.com>
2026-01-31 19:19:42 +0900

    add

---
+++ A/A201.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    string s;
+    getline(cin, s);
+
+    bool inTag = false;
+    string word;
+
+    for (char c : s) {
+        if (inTag) {
+            cout << c;
+            if (c == '>') inTag = false;
+        } else {
+            if (c == '<') {
+                reverse(word.begin(), word.end());
+                cout << word;
+                word.clear();
+
+                inTag = true;
+                cout << c; 
+            } 
+            else if (c == ' ') {
+                reverse(word.begin(), word.end());
+                cout << word;
+                word.clear();
+
+                cout << ' ';
+            } 
+            else {
+                word += c;
+            }
+        }
+    }
+
+    reverse(word.begin(), word.end());
+    cout << word;
+
+    return 0;
+}
